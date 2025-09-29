5d55c7085c726fce1df886b9f438284e160185a5
Emma <emma.malinsdotter@outlook.com>
2025-09-29 13:36:33 +0200

    fixade två små i databasen och i valideringen

--- serverSrc/data/validationProduct.ts
+++ serverSrc/data/validationProduct.ts
@@ -11,7 +11,7 @@ const ProductSchema = z.object({
     id: z.string().min(1, { message: "Produkten måste ha ett ID" }),
     name: z.string().min(1, { message: "Produkten måste ha ett namn" }),
     price: z.number().min(0, { message: "Priset måste ha ett nummer som är noll eller högre" }),
-    imageUrl: z.string().url({ message: "Felaktig URL" }),
+    imageUrl: z.url({ message: "Felaktig URL" }),
     amountInStock: z.number().int({ message: "Amount måste vara ett heltal" }).min(0, { message: "Amount måste vara noll eller högre" }),
 });
 
