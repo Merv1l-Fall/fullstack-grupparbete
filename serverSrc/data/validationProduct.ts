9dda29917706e2fe3829408e57494172f9e011c5
your_username <malahat.m82@gmail.com>
2025-09-30 09:02:07 +0200

    validation products has changed.

--- serverSrc/data/validationProduct.ts
+++ serverSrc/data/validationProduct.ts
@@ -1,12 +1,2 @@
-import { z } from "zod";
 
-export const productSchema = z.object({
-  id: z.string().min(1), 
-  name: z.string().min(1, "Product name is required"),
-  price: z.number().nonnegative("Price must be >= 0"),
-  imageUrl: z.url("Invalid URL").optional(), 
-  amountInStock: z.number().int().nonnegative("Stock must be >= 0")
-});
-
-export type ProductInput = z.infer<typeof productSchema>;
 
