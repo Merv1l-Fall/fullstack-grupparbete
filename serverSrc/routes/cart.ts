e880fec87380e9ea420b99d79d9ca5153b7ce2e6
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-09-30 11:20:08 +0200

    Lagt till tillräcklig Typescript för cart.ts

--- serverSrc/routes/cart.ts
+++ serverSrc/routes/cart.ts
@@ -1,48 +1,59 @@
 //GET PUT POST DELETE etc för cart
 
 import type { Request, Response } from "express";
+import {Router} from "express"
 import express from 'express';
 import fs from "fs"; //-> TODO
 import path from "path"; //-> TODO
 import { cartItemSchema, cartsSchema } from "../data/validationCart.js"
 import { v4 as uuidv4 } from "uuid"; //För unika id:n -> skapar unika id så vi slipper skriva en funktion fört
 
-const router = express.Router();
+export interface CartItem{
+    id: string;
+    productId: string;
+    amount: number;
+}
+export type Cart = CartItem[];
 
-const cartFilePath = path.join(__dirname, "../data/cart.json") //En sökväg till cart.json i data-mappen
+const router: Router = express.Router();
 
-const readCartData = () => {
+const cartFilePath:string = path.join(__dirname, "../data/cart.json") //En sökväg till cart.json i data-mappen
+
+const readCartData = (): CartItem[] => {
   if (!fs.existsSync(cartFilePath)){
     return [];
   }
   const data = fs.readFileSync(cartFilePath, "utf-8");
   if (!data) return [];
   const parsedData = JSON.parse(data);
-  return cartsSchema.parse(parsedData); //-> en array av carts
+  return cartsSchema.parse(parsedData); //-> en array av carts/ typ:CartItem[]
 };
 
-const writeCartData = (data:any) => {
+const writeCartData = (data:CartItem[]): void => {
   fs.writeFileSync(cartFilePath, JSON.stringify(data, null, 2));
 };
 
 //GET - Hämta ALLA Kundvagnar/carts
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response<CartItem[]>) => {
   const carts = readCartData();
   res.status(200).json(carts);
 });
 
 // POST - Skapa en NY kundvagn
 
-router.post("/", (req: Request, res: Response) =>{
+router.post("/", (req: Request<{}, {}, Omit<CartItem, "id">>, res: Response<CartItem | {message: string}>) =>{
   const carts = readCartData();
   const newCart = {...req.body, id: uuidv4()}; //Skapar unikt nytt ID
 
   try{
     cartItemSchema.parse(newCart);
-  } catch(err:any){
-    return res.status(400).json({message: err.errors||err.message});
+  } catch(err:unknown){
+    if (err instanceof Error){
+    return res.status(400).json({message: err.message});
   }
+    return res.status(400).json({message: "Ogiltig indata"});
+}
 
   carts.push(newCart);
   writeCartData(carts);
@@ -51,18 +62,18 @@ router.post("/", (req: Request, res: Response) =>{
 
 //PUT - uppdatera antal för en kundvagn
 
-router.put("/:id", (req: Request, res:Response) => {
+router.put("/:id", (req: Request<{ id: string },{},{ amount: number }>, res:Response<CartItem | {message: string}>) => {
   const carts = readCartData();
   const { id } = req.params;
   const { amount } = req.body;
 
   try{
     cartItemSchema.shape.amount.parse(amount);
-  } catch(err:any){
+  } catch{
     return res.status(400).json({message: "amount måste vara ett POSITIVT heltal"})
   }
 
-  const cart = carts.find((c: any) => c.id === id)
+  const cart = carts.find((c) => c.id === id)
   if (!cart) {
     return res.status(404).json({message: "Kundvagn hittades inte"})
   }
@@ -74,11 +85,11 @@ router.put("/:id", (req: Request, res:Response) => {
 
 //DELETE - Ta bort en kundvagn
 
-router.delete("/:id", (req: Request, res: Response) => {
+router.delete("/:id", (req: Request<{id:string}>, res: Response<{message:string}>) => {
   const carts = readCartData();
   const { id } = req.params;
 
-  const newCarts = carts.filter((c: any) => c.id !== id);
+  const newCarts = carts.filter((c) => c.id !== id);
   if (newCarts.length === carts.length){
     return res.status(404).json({message: "Kundvagnen hittades ej"});
   }
