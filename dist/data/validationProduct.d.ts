d159b64573ac253ca46245b81957fab9bec3905f
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-10-05 22:03:07 +0200

    Tog bort onödiga kommentarer eller dylikt

--- dist/data/validationProduct.d.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export declare const ProductSchema: z.ZodObject<{
-    id: z.ZodString;
-    productName: z.ZodString;
-    price: z.ZodNumber;
-    amountInStock: z.ZodString;
-}, z.core.$strip>;
-export type ProductInput = z.infer<typeof ProductSchema>;
-//# sourceMappingURL=validationProduct.d.ts.map
