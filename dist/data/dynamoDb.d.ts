6708b493d53206ab558571d01844fb1d8921d33f
your_username <malahat.m82@gmail.com>
2025-10-07 12:34:47 +0200

    Stop tracking dist folder

--- dist/data/dynamoDb.d.ts
+++
@@ -1,4 +0,0 @@
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-declare const db: DynamoDBDocumentClient;
-export { db };
-//# sourceMappingURL=dynamoDb.d.ts.map
--- dist/data/types.js
+++
@@ -1,3 +0,0 @@
-// interface/types för users, cart och products
-export {};
-//# sourceMappingURL=types.js.map
